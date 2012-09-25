332cbcf9173a48a48ba8d61ca78c37b98353e97a
David S Anderson <ande3577@gmail.com>
2012-09-24 23:23:23 -0500

    Closes #6: observes dont send to self option

--- app/models/system_notification_mailer.rb
+++ app/models/system_notification_mailer.rb
@@ -1,6 +1,6 @@
 class SystemNotificationMailer < Mailer
   def system_notification(system_notification)
-    @author = User.current
+    @author = User.current()
     @system_notification = system_notification
     mail(:to => system_notification.users.collect(&:mail), :subject => system_notification.subject)
   end
